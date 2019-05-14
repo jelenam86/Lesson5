a7f5db5b42960792bdb25ad349c541864ea6bb69
jelenam86 <jelena.mat86@gmail.com>
2019-05-14 17:17:58 +0200

    Platypus - code added

--- src/optional/Platypus.java
+++ src/optional/Platypus.java
@@ -1,7 +1,17 @@
 package optional;
 
+import java.util.Scanner;
+
 public class Platypus {
 	private String name;
+	
+	public Platypus() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("You have a new pet!");
+		System.out.print("Give them a name: ");
+		this.name = scan.next();
+		scan.close();
+	}
 
 	void sayHi(){
 		System.out.println("The platypus " + name + " is smarter than your average platypus.");
@@ -9,9 +19,9 @@ public class Platypus {
 	
 	public static void main(String[] args) {
 		//1. Make an instance of your new pet platypus
-		
+		Platypus platypus = new Platypus();
 		//2. Call the sayHi method
-		
+		platypus.sayHi();
 		//3. Create a constructor in the platypus class so that you can give your platypus a name.
 	}
 }
