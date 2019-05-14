445ee3711201f7e3d3335d4a959b13badd76fb26
jelenam86 <jelena.mat86@gmail.com>
2019-05-14 16:28:01 +0200

    Added constructor and methods

--- src/examples/Duck.java
+++ src/examples/Duck.java
@@ -10,7 +10,19 @@ public class Duck {
 
 	private String favoriteFood;
 	private int lifeExpectancy;
+	private static int numberOfCreations = 0;
+	private String color;
+
+	{
+		Duck.numberOfCreations++;
+	}
 	
+	public Duck() {
+		if (Duck.numberOfCreations % 2 == 1)
+			this.color = "black";
+		else
+			this.color = "white";
+	}
 
 	public Duck(String favoriteFood, int lifeExpectancy) {
 		this.favoriteFood = favoriteFood;
@@ -26,7 +38,7 @@ public class Duck {
 	public String toString() {
 		return "This duck likes to eat " + this.favoriteFood + " and will live to be " + this.lifeExpectancy + ".";
 	}
-	
+
 	public void quack() {
 		try {
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sounds/quack.wav"));
@@ -37,6 +49,25 @@ public class Duck {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+
+	public static int getNumberOfCreations() {
+		return Duck.numberOfCreations;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	/**
+	 * Only purpose is to properly run testWeCanCountTheNumberOfDucksCreated() in
+	 * StaticDucks class. So with this method we will set numberOfCreations to
+	 * number before entering the test.
+	 * 
+	 * @param a
+	 */
+	public static void setNumberOfCreations(int a) {
+		Duck.numberOfCreations = a;
 	}
 
 }
